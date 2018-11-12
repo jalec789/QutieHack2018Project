325c4f23a19351790ea909b029fcc21170afb55c
Jason Chan <31293853+jalec789@users.noreply.github.com>
2018-11-11 16:51:58 -0800

    Update sketch_nov11a.ino

--- sketch_nov11a.ino
+++ sketch_nov11a.ino
@@ -1,17 +1,13 @@
-void setup() {
-  //set outputs 
-  for(int k=1;k<=13;k++) {
-    pinMode(k, OUTPUT);
-  }
-  //set inputs
-  pinMode(14, INPUT);
-}
 
+//int minOnes=0;
+//int minTens=1;
+//int hrOnes=1;
+//int hrTens=1;
 
 int minOnes=0;
-int minTens=1;
-int hrOnes=0;
-int hrTens=0;
+int minTens=5;
+int hrOnes=6;
+int hrTens=1;
 
 bool runTime(int maxCount, int beginLed, int endLed, int& place) {
   int temp = place;
@@ -42,14 +38,65 @@ bool runTime(int maxCount, int beginLed, int endLed, int& place) {
 }
 
 
+void setup() {
+  Serial.begin(9600);
+  //set outputs 
+  for(int k=1;k<=13;k++) {
+    pinMode(k, OUTPUT);
+  }
+  //set inputs
+  pinMode(14, INPUT);
+
+  runTime(10,1,4,minOnes);
+  runTime(6,5,7,minTens);
+  runTime(10,8,11,hrOnes);
+  runTime(2,12,13,hrTens);
+
+  
+}
+
+
+
 //Test all lights
 void loop() {
-  delay(500);
+  
+  //RUN MINUETS
   if(runTime(10, 1, 4, minOnes)){
     runTime(10, 1, 4, minOnes);
-    if(runTime(5, 5, 7, minTens)){
+    
+    if(runTime(6, 5, 7, minTens)){
+      runTime(6, 5, 7, minTens);
+      
+      
+      //RUN HOURS
+      if(hrTens == 3) {
+        //when the tens place is two count up 3
+        if(runTime(4,8,11,hrOnes)) {
+          runTime(4,8,11,hrOnes);
 
+          if(runTime(3,12,13,hrTens)){
+            runTime(3,12,13,hrTens);
+          }
+        }
+      }
+      else {
+          if(runTime(10,8,11,hrOnes)){
+            runTime(10,8,11, hrOnes);
 
+            if(runTime(3,12,13,hrTens)){
+              runTime(3,12,13,hrTens);
+          }
+        }
+      }
+      
+      
     }
-  } 
+  }
+  Serial.print(hrTens);
+  Serial.print(hrOnes);
+  Serial.print(":");
+  Serial.print(minTens);
+  Serial.print(minOnes);
+  Serial.print("\n");
+  delay(60000);
 }
